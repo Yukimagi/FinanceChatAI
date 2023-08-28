9b99927ad5674173b94fbfa1295f3e9a48f506cb
YuChiLin <yukilin55@gmail.com>
2023-08-29 02:28:37 +0800

    Research Update(2023/8/29)

--- FinanceChatAI/Research1.Designer.cs
+++ FinanceChatAI/Research1.Designer.cs
@@ -29,16 +29,131 @@
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
+            btn_NewsCatch = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            btn_NewsFilter = new Button();
+            label4 = new Label();
+            btn_IntradayReturn = new Button();
+            label5 = new Label();
+            btn_BingChat = new Button();
+            label6 = new Label();
+            btn_Conclusion = new Button();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 3);
+            textBox1.Location = new Point(323, 3);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ScrollBars = ScrollBars.Both;
-            textBox1.Size = new Size(1067, 395);
+            textBox1.Size = new Size(756, 395);
             textBox1.TabIndex = 0;
+            // 
+            // btn_NewsCatch
+            // 
+            btn_NewsCatch.Location = new Point(35, 28);
+            btn_NewsCatch.Name = "btn_NewsCatch";
+            btn_NewsCatch.Size = new Size(126, 29);
+            btn_NewsCatch.TabIndex = 1;
+            btn_NewsCatch.Text = "News Catch";
+            btn_NewsCatch.UseVisualStyleBackColor = true;
+            btn_NewsCatch.Click += btn_NewsCatch_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 6);
+            label1.Name = "label1";
+            label1.Size = new Size(282, 19);
+            label1.TabIndex = 2;
+            label1.Text = "以下是執行此研究的步驟，請按步驟執行:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 33);
+            label2.Name = "label2";
+            label2.Size = new Size(21, 19);
+            label2.TabIndex = 3;
+            label2.Text = "1.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 70);
+            label3.Name = "label3";
+            label3.Size = new Size(21, 19);
+            label3.TabIndex = 4;
+            label3.Text = "2.";
+            // 
+            // btn_NewsFilter
+            // 
+            btn_NewsFilter.Location = new Point(35, 65);
+            btn_NewsFilter.Name = "btn_NewsFilter";
+            btn_NewsFilter.Size = new Size(126, 29);
+            btn_NewsFilter.TabIndex = 5;
+            btn_NewsFilter.Text = "News Filter";
+            btn_NewsFilter.UseVisualStyleBackColor = true;
+            btn_NewsFilter.Click += btn_NewsFilter_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 106);
+            label4.Name = "label4";
+            label4.Size = new Size(21, 19);
+            label4.TabIndex = 6;
+            label4.Text = "3.";
+            // 
+            // btn_IntradayReturn
+            // 
+            btn_IntradayReturn.Location = new Point(35, 101);
+            btn_IntradayReturn.Name = "btn_IntradayReturn";
+            btn_IntradayReturn.Size = new Size(126, 29);
+            btn_IntradayReturn.TabIndex = 7;
+            btn_IntradayReturn.Text = "Intraday Return";
+            btn_IntradayReturn.UseVisualStyleBackColor = true;
+            btn_IntradayReturn.Click += btn_IntradayReturn_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(7, 142);
+            label5.Name = "label5";
+            label5.Size = new Size(21, 19);
+            label5.TabIndex = 8;
+            label5.Text = "4.";
+            // 
+            // btn_BingChat
+            // 
+            btn_BingChat.Location = new Point(34, 137);
+            btn_BingChat.Name = "btn_BingChat";
+            btn_BingChat.Size = new Size(127, 29);
+            btn_BingChat.TabIndex = 9;
+            btn_BingChat.Text = "Bing Chat";
+            btn_BingChat.UseVisualStyleBackColor = true;
+            btn_BingChat.Click += btn_BingChat_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(8, 180);
+            label6.Name = "label6";
+            label6.Size = new Size(21, 19);
+            label6.TabIndex = 10;
+            label6.Text = "5.";
+            // 
+            // btn_Conclusion
+            // 
+            btn_Conclusion.Location = new Point(37, 179);
+            btn_Conclusion.Name = "btn_Conclusion";
+            btn_Conclusion.Size = new Size(124, 29);
+            btn_Conclusion.TabIndex = 11;
+            btn_Conclusion.Text = "Conclusion";
+            btn_Conclusion.UseVisualStyleBackColor = true;
+            btn_Conclusion.Click += btn_Conclusion_Click;
             // 
             // Research1
             // 
@@ -46,6 +161,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1082, 410);
+            Controls.Add(btn_Conclusion);
+            Controls.Add(label6);
+            Controls.Add(btn_BingChat);
+            Controls.Add(label5);
+            Controls.Add(btn_IntradayReturn);
+            Controls.Add(label4);
+            Controls.Add(btn_NewsFilter);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btn_NewsCatch);
             Controls.Add(textBox1);
             Name = "Research1";
             Text = "Research1";
@@ -56,5 +182,16 @@
         #endregion
 
         private TextBox textBox1;
+        private Button btn_NewsCatch;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button btn_NewsFilter;
+        private Label label4;
+        private Button btn_IntradayReturn;
+        private Label label5;
+        private Button btn_BingChat;
+        private Label label6;
+        private Button btn_Conclusion;
     }
 }
--- FinanceChatAI/Research1.cs
+++ FinanceChatAI/Research1.cs
@@ -13,6 +13,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;//1.到“專案” → “加入參考(visualbasic” → “參考管理員”
+                            //2.並最後using就可使用input box
 
 namespace FinanceChatAI
 {
@@ -21,17 +23,111 @@ namespace FinanceChatAI
         public Research1()
         {
             InitializeComponent();
-            InitializeAsync(); // 用異步初始化方法
+            //InitializeAsync(); // 用異步初始化方法
         }
 
+        private void btn_NewsCatch_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("爬整年資料會耗時很久\n如果已有資料(預設是有的)請按否\n不要重跑!\n請問是否繼續?", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                // 設定 Python 檔案的路徑
+                string pythonScriptPath = "news_catch.py";
 
+                // 設定 Python 執行器的路徑(自己去搜尋自己電腦的python.exe的路徑)
+                string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+                // 建立 ProcessStartInfo 物件來設定執行參數
+                ProcessStartInfo psi = new ProcessStartInfo();
+                psi.FileName = pythonInterpreter;
+                psi.Arguments = pythonScriptPath;
+                psi.UseShellExecute = false;
+                psi.CreateNoWindow = true;
+
+                // 執行 Python 腳本
+                using (Process process = new Process())
+                {
+                    process.StartInfo = psi;
+                    process.Start();
+                    process.WaitForExit();
+                }
+            }
+        }
+
+        private void btn_NewsFilter_Click(object sender, EventArgs e)
+        {
+            //此處的py檔有修改(應該會放在.exe同層)，如果您的資料夾沒有我提供的py檔，請向使用者索取
+            string keyword = Interaction.InputBox("輸入查詢的股票(ex: 台積電)", "確定", "台積電", 50, 50);//50,50視窗座標位置
+            string pythonScriptPath = "news_filter.py";
+            string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonInterpreter;
+            psi.Arguments = $"{pythonScriptPath} \"{keyword}\""; // 傳遞關鍵字作為參數
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private void btn_IntradayReturn_Click(object sender, EventArgs e)
+        {
+            //此處的py檔有修改(應該會放在.exe同層)，如果您的資料夾沒有我提供的py檔，請向使用者索取
+            string stockSymbol = Interaction.InputBox("輸入查詢的股票檔案(ex:2330.TW)\n詳情可搜尋yahoo finance", "確定", "2330.TW", 50, 50);//50,50視窗座標位置
+            string pythonScriptPath = "Intraday_return.py";
+            string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonInterpreter;
+            psi.Arguments = $"{pythonScriptPath} \"{stockSymbol}\""; // 傳遞關鍵字作為參數
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+        string month = "1";
+
+        void check()
+        {
+            if (0 < Convert.ToInt32(month) && Convert.ToInt32(month) < 13)
+            {
+                InitializeAsync(); // 用異步初始化方法
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("月份輸入錯誤，請重新輸入，或按取消已結束此動作", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    check();
+                }
+            }
+        }
+        private void btn_BingChat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bing chat聊天會耗時很久\n如果已有資料(預設是有的)請按否\n不要重跑!\n請問是否繼續?", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                month = Interaction.InputBox("請輸入月份(ex:1)", "確定", "1", 50, 50);//50,50視窗座標位置
+                check();
+            }
+        }
         private async void InitializeAsync()
         {
             // 指定 CSV 檔案路徑
             string filePath = "news_answer.csv";
-            string[] news_path = { "2330_1.csv","2330_2.csv","2330_3.csv","2330_4.csv","2330_5.csv",
-                       "2330_6.csv","2330_7.csv","2330_8.csv","2330_9.csv","2330_10.csv",
-                       "2330_11.csv","2330_12.csv" };
+            string[] news_path = { "filtered_1.csv","filtered_2.csv","filtered_3.csv","filtered_4.csv","filtered_5.csv",
+                       "filtered_6.csv","filtered_7.csv","filtered_8.csv","filtered_9.csv","filtered_10.csv",
+                       "filtered_11.csv","filtered_12.csv" };
             string[] ans_path = { "ans_1.csv","ans_2.csv","ans_3.csv","ans_4.csv","ans_5.csv",
                        "ans_6.csv","ans_7.csv","ans_8.csv","ans_9.csv","ans_10.csv",
                        "ans_11.csv","ans_12.csv" };
@@ -133,6 +229,39 @@ namespace FinanceChatAI
                     //sw.Write("The date is: ");
                     //sw.WriteLine(DateTime.Now);
                     i++;
+                }
+            }
+        }
+
+        private void btn_Conclusion_Click(object sender, EventArgs e)
+        {
+            string stockSymbol = Interaction.InputBox("請輸入股票代號(ex:2330)", "確定", "2330", 50, 50);//50,50視窗座標位置
+            string pythonScriptPath = "Conclusion.py";
+            string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonInterpreter;
+            psi.Arguments = $"{pythonScriptPath} \"{stockSymbol}\""; // 傳遞股票代號作為參數
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true; // 捕獲輸出
+            psi.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+
+                // 捕獲 Python 腳本的輸出
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                // 從輸出中解析平均結論值
+                string averageConclusionText = "平均結論值：";
+                int index = output.IndexOf(averageConclusionText);
+                if (index != -1)
+                {
+                    string averageValue = output.Substring(index + averageConclusionText.Length);
+                    MessageBox.Show($"Python 腳本的平均結論值：{averageValue}");
                 }
             }
         }
