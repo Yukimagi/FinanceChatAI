574941b3dd62d437e8484b6b015953ffa1a0c248
YuChiLin <yukilin55@gmail.com>
2023-08-30 02:48:42 +0800

    Forecast update 2023/08/30

--- FinanceChatAI/Forecast1.Designer.cs
+++ FinanceChatAI/Forecast1.Designer.cs
@@ -28,7 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Forecast_label1 = new Label();
+            Forecast_comboBox1 = new ComboBox();
+            Forecast_label2 = new Label();
+            Forecast_textBox1 = new TextBox();
+            Forecast_label3 = new Label();
+            Forecast_label4 = new Label();
+            Forecast_label5 = new Label();
+            Forecast_label6 = new Label();
+            Forecast_label7_point = new Label();
+            Forecast_label8 = new Label();
+            btn_check = new Button();
             SuspendLayout();
+            // 
+            // Forecast_label1
+            // 
+            Forecast_label1.AutoSize = true;
+            Forecast_label1.Font = new Font("Microsoft JhengHei UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label1.ForeColor = Color.Red;
+            Forecast_label1.Location = new Point(12, 9);
+            Forecast_label1.Name = "Forecast_label1";
+            Forecast_label1.Size = new Size(285, 36);
+            Forecast_label1.TabIndex = 0;
+            Forecast_label1.Text = "Auto Forecast Stock";
+            // 
+            // Forecast_comboBox1
+            // 
+            Forecast_comboBox1.FormattingEnabled = true;
+            Forecast_comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
+            Forecast_comboBox1.Location = new Point(291, 54);
+            Forecast_comboBox1.Name = "Forecast_comboBox1";
+            Forecast_comboBox1.Size = new Size(38, 27);
+            Forecast_comboBox1.TabIndex = 1;
+            // 
+            // Forecast_label2
+            // 
+            Forecast_label2.AutoSize = true;
+            Forecast_label2.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label2.Location = new Point(12, 55);
+            Forecast_label2.Name = "Forecast_label2";
+            Forecast_label2.Size = new Size(273, 22);
+            Forecast_label2.TabIndex = 2;
+            Forecast_label2.Text = "請問您想以幾日內的新聞進行預測?";
+            // 
+            // Forecast_textBox1
+            // 
+            Forecast_textBox1.Location = new Point(410, 54);
+            Forecast_textBox1.Multiline = true;
+            Forecast_textBox1.Name = "Forecast_textBox1";
+            Forecast_textBox1.ScrollBars = ScrollBars.Both;
+            Forecast_textBox1.Size = new Size(651, 344);
+            Forecast_textBox1.TabIndex = 3;
+            // 
+            // Forecast_label3
+            // 
+            Forecast_label3.AutoSize = true;
+            Forecast_label3.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label3.Location = new Point(12, 133);
+            Forecast_label3.Name = "Forecast_label3";
+            Forecast_label3.Size = new Size(193, 22);
+            Forecast_label3.TabIndex = 4;
+            Forecast_label3.Text = "正在產生Bing Chat結果";
+            // 
+            // Forecast_label4
+            // 
+            Forecast_label4.AutoSize = true;
+            Forecast_label4.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label4.Location = new Point(12, 93);
+            Forecast_label4.Name = "Forecast_label4";
+            Forecast_label4.Size = new Size(52, 22);
+            Forecast_label4.TabIndex = 5;
+            Forecast_label4.Text = "好的!";
+            // 
+            // Forecast_label5
+            // 
+            Forecast_label5.AutoSize = true;
+            Forecast_label5.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label5.Location = new Point(12, 172);
+            Forecast_label5.Name = "Forecast_label5";
+            Forecast_label5.Size = new Size(239, 22);
+            Forecast_label5.TabIndex = 6;
+            Forecast_label5.Text = "新聞每日預測股票分數已產生!";
+            // 
+            // Forecast_label6
+            // 
+            Forecast_label6.AutoSize = true;
+            Forecast_label6.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label6.Location = new Point(12, 214);
+            Forecast_label6.Name = "Forecast_label6";
+            Forecast_label6.Size = new Size(277, 25);
+            Forecast_label6.TabIndex = 7;
+            Forecast_label6.Text = "您的平均新聞預測股票指數為:";
+            // 
+            // Forecast_label7_point
+            // 
+            Forecast_label7_point.AutoSize = true;
+            Forecast_label7_point.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label7_point.ForeColor = Color.Red;
+            Forecast_label7_point.Location = new Point(87, 273);
+            Forecast_label7_point.Name = "Forecast_label7_point";
+            Forecast_label7_point.Size = new Size(94, 50);
+            Forecast_label7_point.TabIndex = 8;
+            Forecast_label7_point.Text = "100";
+            // 
+            // Forecast_label8
+            // 
+            Forecast_label8.AutoSize = true;
+            Forecast_label8.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label8.ForeColor = Color.Red;
+            Forecast_label8.Location = new Point(231, 273);
+            Forecast_label8.Name = "Forecast_label8";
+            Forecast_label8.Size = new Size(58, 50);
+            Forecast_label8.TabIndex = 9;
+            Forecast_label8.Text = "%";
+            // 
+            // btn_check
+            // 
+            btn_check.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_check.Location = new Point(340, 55);
+            btn_check.Name = "btn_check";
+            btn_check.Size = new Size(64, 29);
+            btn_check.TabIndex = 10;
+            btn_check.Text = "確定!";
+            btn_check.UseVisualStyleBackColor = true;
+            btn_check.Click += btn_check_Click;
             // 
             // Forecast1
             // 
@@ -36,11 +159,35 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1082, 410);
+            Controls.Add(btn_check);
+            Controls.Add(Forecast_label8);
+            Controls.Add(Forecast_label7_point);
+            Controls.Add(Forecast_label6);
+            Controls.Add(Forecast_label5);
+            Controls.Add(Forecast_label4);
+            Controls.Add(Forecast_label3);
+            Controls.Add(Forecast_textBox1);
+            Controls.Add(Forecast_label2);
+            Controls.Add(Forecast_comboBox1);
+            Controls.Add(Forecast_label1);
             Name = "Forecast1";
             Text = "Forecast1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Forecast_label1;
+        private ComboBox Forecast_comboBox1;
+        private Label Forecast_label2;
+        private TextBox Forecast_textBox1;
+        private Label Forecast_label3;
+        private Label Forecast_label4;
+        private Label Forecast_label5;
+        private Label Forecast_label6;
+        private Label Forecast_label7_point;
+        private Label Forecast_label8;
+        private Button btn_check;
     }
 }
--- FinanceChatAI/Forecast1.cs
+++ FinanceChatAI/Forecast1.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using BingChat;
+using CsvHelper.Configuration;
+using CsvHelper;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Linq;
 
 namespace FinanceChatAI
 {
@@ -15,6 +24,248 @@ namespace FinanceChatAI
         public Forecast1()
         {
             InitializeComponent();
+            Forecast_label3.Visible = false;
+            Forecast_label4.Visible = false;
+            Forecast_label5.Visible = false;
+            Forecast_label6.Visible = false;
+            Forecast_label7_point.Visible = false;
+            Forecast_label8.Visible = false;
+
+        }
+
+        void filter()
+        {
+            //此處的py檔有修改(應該會放在.exe同層)，如果您的資料夾沒有我提供的py檔，請向使用者索取
+            string keyword = Interaction.InputBox("輸入查詢的股票(ex: 台積電)", "確定", "台積電", 50, 50);//50,50視窗座標位置
+            string pythonScriptPath = "news_filter_now.py";
+            string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonInterpreter;
+            psi.Arguments = $"{pythonScriptPath} \"{keyword}\""; // 傳遞關鍵字作為參數
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        int daysToKeep = 0;
+        private void btn_check_Click(object sender, EventArgs e)
+        {
+            Forecast_label3.Visible = false;
+            Forecast_label4.Visible = false;
+            Forecast_label5.Visible = false;
+            Forecast_label6.Visible = false;
+            Forecast_label7_point.Visible = false;
+            Forecast_label8.Visible = false;
+
+            DialogResult result = MessageBox.Show("Bing chat聊天會耗時很久\n如果已有資料(預設是有的)請按否\n不要重跑!\n請問是否繼續?", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                daysToKeep = Forecast_comboBox1.SelectedIndex + 1;
+                //Forecast_textBox1.Text = daysToKeep.ToString();
+                Forecast_label4.Visible = true;
+                DialogResult result2 = MessageBox.Show("請問是否要開始爬資料\n如果已有資料(預設是有的)請按否\n不要重跑!\n請問是否繼續?", "注意", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result2 == DialogResult.Yes)
+                {
+                    // 設定 Python 檔案的路徑
+                    string pythonScriptPath = "catch_news_now.py";
+
+                    // 設定 Python 執行器的路徑(自己去搜尋自己電腦的python.exe的路徑)
+                    string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+                    // 建立 ProcessStartInfo 物件來設定執行參數
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.FileName = pythonInterpreter;
+                    psi.Arguments = pythonScriptPath;
+                    psi.UseShellExecute = false;
+                    psi.CreateNoWindow = true;
+
+                    // 執行 Python 腳本
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo = psi;
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                    filter();
+
+                }
+                InitializeAsync(); // 用異步初始化方法
+            }
+        }
+
+        private async void InitializeAsync()
+        {
+            Forecast_label3.Visible = true;
+            // 指定 CSV 檔案路徑
+            string news_path = "news_now_filter.csv";
+            string ans_path = "ans_forcast.csv";
+
+            using (StreamWriter writer = new StreamWriter(ans_path, false, Encoding.UTF8)) // 使用 UTF-8 編碼
+            {
+                writer.WriteLine("date,title, Y/N");
+            }
+
+            // 定義資料類別以匹配 CSV 檔案中的結構
+            int num = 0;
+            int num2 = 0;
+
+            var record0 = new List<Dictionary<string, string>>();
+            var records = new List<Dictionary<string, string>>();
+            var dates = new HashSet<string>(); // 使用 HashSet 来存储不重复的日期
+            // 讀取 2330_2022.csv(or每個月) 檔案
+            using (var reader = new StreamReader(news_path))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                csv.Read();
+                csv.ReadHeader();
+
+                while (csv.Read())
+                {
+                    var record = new Dictionary<string, string>();
+                    foreach (var header in csv.HeaderRecord)
+                    {
+                        record[header] = csv.GetField(header);
+                    }
+
+                    string date = record["date"];
+                    if (!dates.Contains(date))
+                    {
+                        dates.Add(date); // 添加新的日期
+                        if (dates.Count > daysToKeep)
+                        {
+                            break; // 如果日期数量超过了 daysToKeep，跳出循环
+                        }
+                    }
+
+                    record0.Add(record);
+                    
+                }
+            }
+
+            foreach (var date in dates)
+            {
+                foreach (var record in record0)
+                {
+                    if (record["date"] == date)
+                    {
+                        Forecast_textBox1.AppendText("\n" + $"Date: {record["date"]}, Title: {record["title"]}" + Environment.NewLine + Environment.NewLine);
+                        records.Add(record);
+                        num2++;
+                    }
+                }
+            }
+        
+            string cookieFilePath = "bing_cookies_myaccount.json";
+            var client = new BingChatClient(new BingChatClientOptions
+            {
+                //如果你遇到可能不行一直問問題的情況，就需要連上cookie(cookie可能會於跳掉網頁後更新)
+                //設置自己的 Cookie
+                //您可以設置自己的 Cookie，以與 BingChat 伺服器互動，而不是隨機生成一個。
+                //要獲取 Cookie，您可以前往 www.bing.com，登錄一個有權限的帳戶，然後打開開發人員工具（F12）> 應用程序標籤 > 存儲 > Cookies，在名為 "_U" 的 Cookie 中查找並複製其值。然後將其傳遞給 BingChatClientOptions 的構造函數。
+
+                //CookieU = "1o499k1A7QHlmVyL9_N5TFly3djBOO1b50fNiRcSRQlhDVcBDOIHEAHQxwib3us4kF0dNcauIo4_sXbmVEfed682jowhjmA6YUcP6o6NUFxXzJWn-ikeNZNRqMNlbuYoziVlWj6rL_r6XjeP9bgSNeboLpyaMWfOXEdMuL_t9vxfsWvxHnf2Ijp06DcSReiVN8k0fUxy4TZ3f3X-wWR2Mjw1kbEcOKPysdT2Y-H1DQUY",
+                // Tone used for conversation
+                Tone = BingChatTone.Balanced,
+                CookieFilePath = cookieFilePath
+                //CookieKievRPSSecAuth = "FAByBBRaTOJILtFsMkpLVWSG6AN6C/svRwNmAAAEgAAACLpd12T3FdQBMATGoyc293Bh9nJ1mK2vpk+jh4Mp/YqXomqhViuTDv+R9WN7qJ2b1ac8aGalzFesGRcp0FtOSQOShMrXBV8KHAuWlG7IbiXU5j4zH8JyE25mM2ADiaCBoA11vsTFmz5LaaiB6ztra/2d3V7NvreyehurX/wdswwXTMxDEAmEPLX/wuuYmGDdldKZ4wAc6ZnCWS7RJpSoad9MezDKcGjCzlCg5u8vcVAZyrCH5mT3g3TItSNiDhc0rOM9mPrfBk7FsB4/Fgm5l21kiSLWVZH/uERzsBOLTtjcwa/Meo0y50eKNXLNpbHTQ2u9OwFv9x+196K5UKGjx12LPH3IrUPvsKAyfgLJy9gzIxrQSCz54Iwf142Jm4scalcGQTqMOlxSagYnJQdQhw4z/KeqVzgtR7KHCT0r49uK5PSh5oOLNkwOMjl2jwHhks3qsdyBtDmU0CL6iVqOMk1oE9wXOXh5eG2iT/jrEvyAe+pGEmv+lB73ItV9o3DCqqURZVZ3mfnlybp9rSGp06c0CAL4xdujVVuWlTbh7Hy2OIWeusOfjMpBpP+if1G9dK3tJRD9GF31PpkqOy2ZMsO4xIagyueF+dc6UTNCaDtxI5j1GwFxDUphzJuUwuHzDPCUJBqqsJjGK6Z8ojKOabKcy0mC+X1oE4LX++f//BN0Jr6V8zuHEx9CBZZSefQuyry2h9tPC47hYs/Qd96BZggDHTSnBHkX5FEVOo064qMdtkXFN1kbk977DqLKu1Bi/ossv7jOopkUZA8Idq0vRIv4c2KuXmy2w8Hb34w26HmpRzpJuBdw/ly4VVO0MpjT903E1wxSBRwr7096ZGU8d9B+TQXCa4nEjjmKXP3jAQtobl/fIKqPArxAmU5R4lO1eKnoR/6ROShGnwjh4ppsQYu5De1LxN0q9M/54rCbFvqMfHE5r5aKufLALoW3OAjBq6C1jZZ00Up1MvEHSxGDBy1TkhYn3bojwr+2OfA3soTG9ztYJpeJvYv0kDmMq+IIuFaVT/nE2+sQV6WmGlOIpDkQQaFMGimI4cIdgH+GJ/2lOEp94vS5SOHmR7OHguHNtAhDL9Vju2kl2jlTI7h0BjmYb7Ww/8IXC/OTEU6CiyN8/9Xp9OHlPdlkKFYOBpz6bfRjMNV+fqTG7vApa6t83ojPaxXj0GqPY+UofU/GsDAo4lgDP71vfFtdfCJV4FgGELhzfiu8BphgzWrVDjlSDojInCfmZoCa/Hcnelz7uxLSLD9YGJVjkNKpzh3rRaFVdnAUu0iSuIJDS+NPbOwcr1lNoZQHS+Q/Vd6sTZ7Gt8DPyy4gi3EA+YqO0yISjN/51CBPntXX92TdsXtt4A+wVZy0PPBcbBiaRWdqgfhPcr701cTN03Gt4BLwEXwFTZ3CfOn6ja+cE/oEvWOq4ReFpY0WJQVvt+xcA87/FACMMhx+6pwIko+H9pQ0/OvzMucbrg==",
+            });
+            //驗證captcha
+            Process.Start(new ProcessStartInfo("cmd", $"/c start https://www.bing.com/turing/captcha/challenge") { CreateNoWindow = true });
+            Thread.Sleep(1000);
+            int i = 0;
+            //string[] t = { "年底確定來不及！台積電中科2奈米廠用地延至明年交地" , "慘？市場傳三度調降財測 台積電急重訊澄清" };
+            string[] answer = new string[num2];
+            foreach (var record in records)
+            {
+                //假設你是一位財務專家而且您是一位具有股票推薦經驗的金融專家，如果以下新聞標題是好消息請只回答#yes，而如果是壞消息則只回答#no，如果不確定則只回答#unknown，然後於下一行用簡短的句子進行詳細說明，這個標題對公司股價是好還是壞呢? 而以下為要分析的新聞標題   
+                var message = "假設你是一位財務專家而且您是一位具有股票推薦經驗的金融專家，如果以下新聞標題是好消息請只回答#yes，而如果是壞消息則只回答#no，如果不確定則只回答#unknown，然後於下一行用簡短的句子進行詳細說明，這個標題對公司股價是好還是壞呢? 而以下為要分析的新聞標題:" + record["title"];
+                Forecast_textBox1.AppendText(message + Environment.NewLine + Environment.NewLine);
+
+                answer[i] = await client.AskAsync(message);
+
+                Forecast_textBox1.AppendText($"Answer: {answer[i]}" + Environment.NewLine + Environment.NewLine);
+                //Console.WriteLine($"Answer: {answer[i]}");
+
+                string pattern = @"(#unknown|#no|#yes)";
+
+                MatchCollection matches = Regex.Matches(answer[i], pattern);
+
+                foreach (Match match in matches)
+                {
+                    string keywordText = match.Value;
+                    // 寫入 CSV 資料行
+                    using (StreamWriter writer = new StreamWriter(ans_path, true, Encoding.UTF8)) // 第二個參數為 true，表示追加模式
+                    {
+                        writer.WriteLine(record["date"] + "," + record["title"] + "," + keywordText);
+                    }
+                }
+
+                i++;
+            }
+
+            // Create an instance of StreamWriter to write text to a file.
+            // The using statement also closes the StreamWriter.
+            using (StreamWriter sw = new StreamWriter("output_forcast.TXT"))   //小寫TXT     
+            {
+                i = 0;
+                foreach (var record in records)
+                {
+                    // Add some text to the file.
+                    sw.WriteLine(record["date"]);
+                    sw.WriteLine(record["title"]);
+                    sw.WriteLine(answer[i]);
+                    // Arbitrary objects can also be written to the file.
+                    //sw.Write("The date is: ");
+                    //sw.WriteLine(DateTime.Now);
+                    i++;
+                }
+            }
+            Forecast_label3.Visible = false;
+            conclusion();
+        }
+        void conclusion()
+        {
+            Forecast_label5.Visible = true;
+            Forecast_label6.Visible = true;
+            //此處的py檔有修改(應該會放在.exe同層)，如果您的資料夾沒有我提供的py檔，請向使用者索取
+            string stockSymbol = Interaction.InputBox("請輸入股票代號(ex:2330)", "確定", "2330", 50, 50);//50,50視窗座標位置
+            string pythonScriptPath = "Conclusion_Forecast.py";
+            string pythonInterpreter = "C:\\Users\\USER\\AppData\\Local\\Programs\\Python\\Python311\\python.exe";
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = pythonInterpreter;
+            psi.Arguments = $"{pythonScriptPath} \"{stockSymbol}\""; // 傳遞股票代號作為參數
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true; // 捕獲輸出
+            psi.CreateNoWindow = true;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+
+                // 捕獲 Python 腳本的輸出
+                string output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                // 從輸出中解析平均結論值
+                string averageConclusionText = "平均結論值：";
+                int index = output.IndexOf(averageConclusionText);
+                if (index != -1)
+                {
+                    string averageValue = output.Substring(index + averageConclusionText.Length);
+                    Forecast_label7_point.Text = (averageValue + "");
+                    Forecast_label7_point.Visible = true;
+                    Forecast_label8.Visible = true;
+                }
+            }
         }
     }
 }
