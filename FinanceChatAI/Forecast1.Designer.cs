6cd923bd5465e2b375de4fb0077de394ebf61514
YuChiLin <yukilin55@gmail.com>
2023-08-30 12:02:03 +0800

    label renew 2 (2023/08/30)

--- FinanceChatAI/Forecast1.Designer.cs
+++ FinanceChatAI/Forecast1.Designer.cs
@@ -45,11 +45,11 @@
             // Forecast_label1
             // 
             Forecast_label1.AutoSize = true;
-            Forecast_label1.Font = new Font("Microsoft JhengHei UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label1.Font = new Font("Microsoft JhengHei UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
             Forecast_label1.ForeColor = Color.Red;
-            Forecast_label1.Location = new Point(12, 9);
+            Forecast_label1.Location = new Point(190, 9);
             Forecast_label1.Name = "Forecast_label1";
-            Forecast_label1.Size = new Size(285, 36);
+            Forecast_label1.Size = new Size(377, 47);
             Forecast_label1.TabIndex = 0;
             Forecast_label1.Text = "Auto Forecast Stock";
             // 
@@ -57,7 +57,7 @@
             // 
             Forecast_comboBox1.FormattingEnabled = true;
             Forecast_comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
-            Forecast_comboBox1.Location = new Point(206, 80);
+            Forecast_comboBox1.Location = new Point(450, 92);
             Forecast_comboBox1.Name = "Forecast_comboBox1";
             Forecast_comboBox1.Size = new Size(38, 27);
             Forecast_comboBox1.TabIndex = 1;
@@ -65,88 +65,88 @@
             // Forecast_label2
             // 
             Forecast_label2.AutoSize = true;
-            Forecast_label2.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label2.Location = new Point(12, 55);
+            Forecast_label2.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label2.Location = new Point(190, 59);
             Forecast_label2.Name = "Forecast_label2";
-            Forecast_label2.Size = new Size(268, 22);
+            Forecast_label2.Size = new Size(363, 29);
             Forecast_label2.TabIndex = 2;
             Forecast_label2.Text = "請問您想以近10天內過濾的資料中";
             // 
             // Forecast_textBox1
             // 
-            Forecast_textBox1.Location = new Point(356, -4);
+            Forecast_textBox1.Location = new Point(688, 9);
             Forecast_textBox1.Multiline = true;
             Forecast_textBox1.Name = "Forecast_textBox1";
             Forecast_textBox1.ScrollBars = ScrollBars.Both;
-            Forecast_textBox1.Size = new Size(1035, 392);
+            Forecast_textBox1.Size = new Size(713, 421);
             Forecast_textBox1.TabIndex = 3;
             // 
             // Forecast_label3
             // 
             Forecast_label3.AutoSize = true;
-            Forecast_label3.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label3.Location = new Point(12, 150);
+            Forecast_label3.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label3.Location = new Point(190, 143);
             Forecast_label3.Name = "Forecast_label3";
-            Forecast_label3.Size = new Size(193, 22);
+            Forecast_label3.Size = new Size(276, 29);
             Forecast_label3.TabIndex = 4;
-            Forecast_label3.Text = "正在產生Bing Chat結果";
+            Forecast_label3.Text = "正在產生Bing Chat結果...";
             // 
             // Forecast_label4
             // 
             Forecast_label4.AutoSize = true;
-            Forecast_label4.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label4.Location = new Point(12, 110);
+            Forecast_label4.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label4.Location = new Point(612, 92);
             Forecast_label4.Name = "Forecast_label4";
-            Forecast_label4.Size = new Size(52, 22);
+            Forecast_label4.Size = new Size(70, 29);
             Forecast_label4.TabIndex = 5;
             Forecast_label4.Text = "好的!";
             // 
             // Forecast_label5
             // 
             Forecast_label5.AutoSize = true;
-            Forecast_label5.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label5.Location = new Point(12, 189);
+            Forecast_label5.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label5.Location = new Point(190, 190);
             Forecast_label5.Name = "Forecast_label5";
-            Forecast_label5.Size = new Size(239, 22);
+            Forecast_label5.Size = new Size(323, 29);
             Forecast_label5.TabIndex = 6;
             Forecast_label5.Text = "新聞每日預測股票分數已產生!";
             // 
             // Forecast_label6
             // 
             Forecast_label6.AutoSize = true;
-            Forecast_label6.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label6.Location = new Point(12, 231);
+            Forecast_label6.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label6.Location = new Point(195, 232);
             Forecast_label6.Name = "Forecast_label6";
-            Forecast_label6.Size = new Size(277, 25);
+            Forecast_label6.Size = new Size(318, 29);
             Forecast_label6.TabIndex = 7;
             Forecast_label6.Text = "您的平均新聞預測股票指數為:";
             // 
             // Forecast_label7_point
             // 
             Forecast_label7_point.AutoSize = true;
-            Forecast_label7_point.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label7_point.Font = new Font("Microsoft JhengHei UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
             Forecast_label7_point.ForeColor = Color.Red;
-            Forecast_label7_point.Location = new Point(87, 273);
+            Forecast_label7_point.Location = new Point(308, 294);
             Forecast_label7_point.Name = "Forecast_label7_point";
-            Forecast_label7_point.Size = new Size(94, 50);
+            Forecast_label7_point.Size = new Size(109, 60);
             Forecast_label7_point.TabIndex = 8;
             Forecast_label7_point.Text = "100";
             // 
             // Forecast_label8
             // 
             Forecast_label8.AutoSize = true;
-            Forecast_label8.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label8.Font = new Font("Microsoft JhengHei UI", 28.2F, FontStyle.Bold, GraphicsUnit.Point);
             Forecast_label8.ForeColor = Color.Red;
-            Forecast_label8.Location = new Point(231, 273);
+            Forecast_label8.Location = new Point(513, 294);
             Forecast_label8.Name = "Forecast_label8";
-            Forecast_label8.Size = new Size(58, 50);
+            Forecast_label8.Size = new Size(67, 60);
             Forecast_label8.TabIndex = 9;
             Forecast_label8.Text = "%";
             // 
             // btn_check
             // 
             btn_check.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btn_check.Location = new Point(286, 70);
+            btn_check.Location = new Point(516, 90);
             btn_check.Name = "btn_check";
             btn_check.Size = new Size(64, 29);
             btn_check.TabIndex = 10;
@@ -157,10 +157,10 @@
             // Forecast_label9
             // 
             Forecast_label9.AutoSize = true;
-            Forecast_label9.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label9.Location = new Point(12, 81);
+            Forecast_label9.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label9.Location = new Point(190, 90);
             Forecast_label9.Name = "Forecast_label9";
-            Forecast_label9.Size = new Size(188, 22);
+            Forecast_label9.Size = new Size(254, 29);
             Forecast_label9.TabIndex = 11;
             Forecast_label9.Text = "幾日內的新聞進行預測?";
             // 
--- FinanceChatAI/Research1.Designer.cs
+++ FinanceChatAI/Research1.Designer.cs
@@ -44,18 +44,19 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(323, 3);
+            textBox1.Location = new Point(673, 12);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ScrollBars = ScrollBars.Both;
-            textBox1.Size = new Size(1068, 395);
+            textBox1.Size = new Size(718, 415);
             textBox1.TabIndex = 0;
             // 
             // btn_NewsCatch
             // 
-            btn_NewsCatch.Location = new Point(35, 28);
+            btn_NewsCatch.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_NewsCatch.Location = new Point(252, 95);
             btn_NewsCatch.Name = "btn_NewsCatch";
-            btn_NewsCatch.Size = new Size(126, 29);
+            btn_NewsCatch.Size = new Size(243, 37);
             btn_NewsCatch.TabIndex = 1;
             btn_NewsCatch.Text = "News Catch";
             btn_NewsCatch.UseVisualStyleBackColor = true;
@@ -64,35 +65,39 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 6);
+            label1.Font = new Font("Microsoft JhengHei UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(88, 9);
             label1.Name = "label1";
-            label1.Size = new Size(282, 19);
+            label1.Size = new Size(512, 50);
             label1.TabIndex = 2;
-            label1.Text = "以下是執行此研究的步驟，請按步驟執行:";
+            label1.Text = "執行此研究，請按步驟執行:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(6, 33);
+            label2.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(200, 103);
             label2.Name = "label2";
-            label2.Size = new Size(21, 19);
+            label2.Size = new Size(33, 29);
             label2.TabIndex = 3;
             label2.Text = "1.";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(6, 70);
+            label3.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(200, 144);
             label3.Name = "label3";
-            label3.Size = new Size(21, 19);
+            label3.Size = new Size(33, 29);
             label3.TabIndex = 4;
             label3.Text = "2.";
             // 
             // btn_NewsFilter
             // 
-            btn_NewsFilter.Location = new Point(35, 65);
+            btn_NewsFilter.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_NewsFilter.Location = new Point(253, 140);
             btn_NewsFilter.Name = "btn_NewsFilter";
-            btn_NewsFilter.Size = new Size(126, 29);
+            btn_NewsFilter.Size = new Size(242, 37);
             btn_NewsFilter.TabIndex = 5;
             btn_NewsFilter.Text = "News Filter";
             btn_NewsFilter.UseVisualStyleBackColor = true;
@@ -101,17 +106,19 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 106);
+            label4.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(200, 188);
             label4.Name = "label4";
-            label4.Size = new Size(21, 19);
+            label4.Size = new Size(33, 29);
             label4.TabIndex = 6;
             label4.Text = "3.";
             // 
             // btn_IntradayReturn
             // 
-            btn_IntradayReturn.Location = new Point(35, 101);
+            btn_IntradayReturn.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_IntradayReturn.Location = new Point(253, 183);
             btn_IntradayReturn.Name = "btn_IntradayReturn";
-            btn_IntradayReturn.Size = new Size(126, 29);
+            btn_IntradayReturn.Size = new Size(242, 38);
             btn_IntradayReturn.TabIndex = 7;
             btn_IntradayReturn.Text = "Intraday Return";
             btn_IntradayReturn.UseVisualStyleBackColor = true;
@@ -120,17 +127,19 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(7, 142);
+            label5.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(200, 232);
             label5.Name = "label5";
-            label5.Size = new Size(21, 19);
+            label5.Size = new Size(33, 29);
             label5.TabIndex = 8;
             label5.Text = "4.";
             // 
             // btn_BingChat
             // 
-            btn_BingChat.Location = new Point(34, 137);
+            btn_BingChat.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_BingChat.Location = new Point(253, 227);
             btn_BingChat.Name = "btn_BingChat";
-            btn_BingChat.Size = new Size(127, 29);
+            btn_BingChat.Size = new Size(242, 38);
             btn_BingChat.TabIndex = 9;
             btn_BingChat.Text = "Bing Chat";
             btn_BingChat.UseVisualStyleBackColor = true;
@@ -139,17 +148,19 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(8, 180);
+            label6.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(200, 276);
             label6.Name = "label6";
-            label6.Size = new Size(21, 19);
+            label6.Size = new Size(33, 29);
             label6.TabIndex = 10;
             label6.Text = "5.";
             // 
             // btn_Conclusion
             // 
-            btn_Conclusion.Location = new Point(37, 179);
+            btn_Conclusion.Font = new Font("Microsoft JhengHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            btn_Conclusion.Location = new Point(253, 271);
             btn_Conclusion.Name = "btn_Conclusion";
-            btn_Conclusion.Size = new Size(124, 29);
+            btn_Conclusion.Size = new Size(242, 38);
             btn_Conclusion.TabIndex = 11;
             btn_Conclusion.Text = "Conclusion";
             btn_Conclusion.UseVisualStyleBackColor = true;
