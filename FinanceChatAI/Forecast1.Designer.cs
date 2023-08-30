561f9e91a27b1082a59f816ea42513ab97f396f8
YuChiLin <yukilin55@gmail.com>
2023-08-30 11:48:27 +0800

    label renew 2023/08/30

--- FinanceChatAI/Forecast1.Designer.cs
+++ FinanceChatAI/Forecast1.Designer.cs
@@ -39,6 +39,7 @@
             Forecast_label7_point = new Label();
             Forecast_label8 = new Label();
             btn_check = new Button();
+            Forecast_label9 = new Label();
             SuspendLayout();
             // 
             // Forecast_label1
@@ -56,7 +57,7 @@
             // 
             Forecast_comboBox1.FormattingEnabled = true;
             Forecast_comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" });
-            Forecast_comboBox1.Location = new Point(291, 54);
+            Forecast_comboBox1.Location = new Point(206, 80);
             Forecast_comboBox1.Name = "Forecast_comboBox1";
             Forecast_comboBox1.Size = new Size(38, 27);
             Forecast_comboBox1.TabIndex = 1;
@@ -67,24 +68,24 @@
             Forecast_label2.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             Forecast_label2.Location = new Point(12, 55);
             Forecast_label2.Name = "Forecast_label2";
-            Forecast_label2.Size = new Size(273, 22);
+            Forecast_label2.Size = new Size(268, 22);
             Forecast_label2.TabIndex = 2;
-            Forecast_label2.Text = "請問您想以幾日內的新聞進行預測?";
+            Forecast_label2.Text = "請問您想以近10天內過濾的資料中";
             // 
             // Forecast_textBox1
             // 
-            Forecast_textBox1.Location = new Point(410, -4);
+            Forecast_textBox1.Location = new Point(356, -4);
             Forecast_textBox1.Multiline = true;
             Forecast_textBox1.Name = "Forecast_textBox1";
             Forecast_textBox1.ScrollBars = ScrollBars.Both;
-            Forecast_textBox1.Size = new Size(981, 392);
+            Forecast_textBox1.Size = new Size(1035, 392);
             Forecast_textBox1.TabIndex = 3;
             // 
             // Forecast_label3
             // 
             Forecast_label3.AutoSize = true;
             Forecast_label3.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label3.Location = new Point(12, 133);
+            Forecast_label3.Location = new Point(12, 150);
             Forecast_label3.Name = "Forecast_label3";
             Forecast_label3.Size = new Size(193, 22);
             Forecast_label3.TabIndex = 4;
@@ -94,7 +95,7 @@
             // 
             Forecast_label4.AutoSize = true;
             Forecast_label4.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label4.Location = new Point(12, 93);
+            Forecast_label4.Location = new Point(12, 110);
             Forecast_label4.Name = "Forecast_label4";
             Forecast_label4.Size = new Size(52, 22);
             Forecast_label4.TabIndex = 5;
@@ -104,7 +105,7 @@
             // 
             Forecast_label5.AutoSize = true;
             Forecast_label5.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label5.Location = new Point(12, 172);
+            Forecast_label5.Location = new Point(12, 189);
             Forecast_label5.Name = "Forecast_label5";
             Forecast_label5.Size = new Size(239, 22);
             Forecast_label5.TabIndex = 6;
@@ -114,7 +115,7 @@
             // 
             Forecast_label6.AutoSize = true;
             Forecast_label6.Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            Forecast_label6.Location = new Point(12, 214);
+            Forecast_label6.Location = new Point(12, 231);
             Forecast_label6.Name = "Forecast_label6";
             Forecast_label6.Size = new Size(277, 25);
             Forecast_label6.TabIndex = 7;
@@ -145,7 +146,7 @@
             // btn_check
             // 
             btn_check.Font = new Font("Microsoft JhengHei UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            btn_check.Location = new Point(340, 55);
+            btn_check.Location = new Point(286, 70);
             btn_check.Name = "btn_check";
             btn_check.Size = new Size(64, 29);
             btn_check.TabIndex = 10;
@@ -153,12 +154,23 @@
             btn_check.UseVisualStyleBackColor = true;
             btn_check.Click += btn_check_Click;
             // 
+            // Forecast_label9
+            // 
+            Forecast_label9.AutoSize = true;
+            Forecast_label9.Font = new Font("Microsoft JhengHei UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            Forecast_label9.Location = new Point(12, 81);
+            Forecast_label9.Name = "Forecast_label9";
+            Forecast_label9.Size = new Size(188, 22);
+            Forecast_label9.TabIndex = 11;
+            Forecast_label9.Text = "幾日內的新聞進行預測?";
+            // 
             // Forecast1
             // 
             AutoScaleDimensions = new SizeF(9F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1403, 390);
+            Controls.Add(Forecast_label9);
             Controls.Add(btn_check);
             Controls.Add(Forecast_label8);
             Controls.Add(Forecast_label7_point);
@@ -189,5 +201,6 @@
         private Label Forecast_label7_point;
         private Label Forecast_label8;
         private Button btn_check;
+        private Label Forecast_label9;
     }
 }
